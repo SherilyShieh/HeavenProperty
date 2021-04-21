a9d0b1cbf21b1d3488fbb80bf519d544148fdf5c
sherily <1252524051@qq.com>
2021-04-21 13:31:10 +1200

    init

---
+++ HeavenProperty/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeavenProperty.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
